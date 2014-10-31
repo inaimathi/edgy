b2547da52743e58b88ed635e41f837a53ad5fb63
inaimathi <inaimathi@self>
2014-10-31 10:12:50 -0400

    -Implemented full thinning and thinned sorting. Works pretty well (though there's no difference between circles and squares yet)

--- Multicolor.hs
+++ Multicolor.hs
@@ -18,9 +18,21 @@ getDirections ms = concatMap (direct [cardinal, ordinal]) ms
     where direct fns m = let scored = scoreMap m
                          in map (\fn -> Map.map fn scored) fns
 
+getLines :: Map Coord a -> [Map Coord Direction]
+getLines m = recur . byThinnedLen . map toInternal . concatMap (islands 7) . concatMap splitByVal $ getDirections [m]
+    where toInternal region = (region, thinRegion region)
+          byThinnedLen = sortBy (flip compare `on` (Map.size . snd))
+          recur [] = []
+          recur (r:rest) = (snd r) : (recur $ filterOut (fst r) rest)
+          filterOut m [] = []
+          filterOut m (r:rest) = let filtered = Map.difference (fst r) m
+                                 in if Map.size filtered > 7 
+                                    then (filtered, snd r) : (filterOut m rest)
+                                    else filterOut m rest
+
 main :: IO ()
 main = do f <- readSparse "multi.txt"
           putBeside . map showMap . getDirections $ separate 7 f
-          let square = getDirections [last $ separate 7 f]
---          putBeside . (map showMap) $ sortBy (compare `on` sizeI) $ concatMap (islands 7) $ concatMap splitByVal square
-          mapM_ putBeside . splitEvery 6 . map showMap . map thinRegion . concatMap (islands 7) . concatMap splitByVal . getDirections $ separate 7 f
+          let thinnedLines = concatMap getLines $ separate 7 f
+          putBeside $ map showMap thinnedLines
+          putStrLn . showMap $ Map.unions thinnedLines
