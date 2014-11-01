15b3b33cb760ba6f3740da643d2302fabb0a955f
inaimathi <inaimathi@self>
2014-11-01 15:09:33 -0400

    -Cleaned up some of Elements -Added output SVG files

--- Elements.hs
+++ Elements.hs
@@ -1,5 +1,6 @@
-module Elements ( Element, computeElements
+module Elements ( Element, getElements
                 , fbShow, fbWrite
+                , svgWrite
                 , thinLines) where
 
 import Util
@@ -7,38 +8,14 @@ import Model
 import SparseRead
 import Direction
 
+import System.FilePath (replaceExtension)
 import qualified Data.Maybe as Maybe 
 import Data.Function (on)
 import Data.List (sortBy)
-import Data.Map (Map)
 import qualified Data.Map as Map
 
 data Element = Line Coord Coord deriving (Eq, Ord, Show, Read)
-class ToSVG a where
-    toSVG :: a -> String
 
-instance ToSVG Element where
-    toSVG (Line (x, y) (x', y')) = concat ["<line x1=", ss x, " y1=", ss y, " x2=", ss x', " y2=", ss y', " stroke-width=\"2\"/>"]
-        where ss = show . show . (*10)
-
-svgWrite :: FilePath -> [Element] -> IO ()
-svgWrite fname elems = writeFile fname contents
-    where contents = concat [ "<svg width=\"12cm\" height=\"10cm\" viewBox=\"0 0 1200 1000\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"
-                            , "<g stroke=\"green\">"
-                            , concatMap toSVG elems
-                            , "</g></svg>" ]
-
-fbShow :: String -> Element -> String
-fbShow factId (Line (x, y) (x', y')) = 
-    concat [ "(", factId, " :LINE-SEGMENT NIL)\n"
-           , "(", factId, " :START (", show x, " ", show y, "))\n"
-           , "(", factId, " :END (", show x', " ", show y', "))\n"]
-
-fbWrite :: FilePath -> [Element] -> IO ()
-fbWrite fname elems = writeFile fname . concat $ map (uncurry fbShow) pairs
-    where pairs = zip (map ((":FACT"++) . show) [1..]) elems
-
----------- Post-generation Modification
 align :: [Element] -> [Element]
 align es = foldl (\memo pt -> alignY pt memo) xAligned pts
     where xAligned = foldl (\memo pt -> alignX pt memo) es pts 
@@ -54,11 +31,11 @@ align es = foldl (\memo pt -> alignY pt memo) xAligned pts
                                                        then (x, y)
                                                        else (x', y)
 
-resolveOverlaps :: [Element] -> [Element]
--- TODO; should combine/filter overlapping lines.
-resolveOverlaps es = undefined
+-- resolveOverlaps :: [Element] -> [Element]
+-- -- TODO; should combine/filter overlapping lines.
+-- resolveOverlaps es = undefined
 
----------- Line-based thinning
+---------- Line-based thinning and element generation
 thinLines :: Grid Direction -> Maybe Element
 thinLines m
     | Map.null m = Nothing
@@ -83,7 +60,6 @@ thinLines m
                         ln y = [(x, y) | x <- [minX..maxX]]
                         col x = [(x, y) | y <- [minY..maxY]]
 
----------- Line-based generation
 computeElements :: Integer -> Grid a -> [Element]
 computeElements threshold m = recur . byDistance . Maybe.mapMaybe toInternal . concatMap (islands threshold) . concatMap splitByVal $ getDirections m
     where toInternal region = case thinLines region of
@@ -100,12 +76,38 @@ computeElements threshold m = recur . byDistance . Maybe.mapMaybe toInternal . c
                                       (True, Just ln) -> (filtered, ln) : (filterOut m rest)
                                       _ -> filterOut m rest
 
-computeFromGrid :: Ord a => Integer -> Grid a -> [Element]
-computeFromGrid threshold g = concatMap (computeElements threshold) . concatMap (islands threshold) $ splitByVal g
+getElements :: Ord a => Integer -> Grid a -> [Element]
+getElements threshold g = concatMap (computeElements threshold) . concatMap (islands threshold) $ splitByVal g
+
+---------- File emission
+svgShow :: Element -> String
+svgShow (Line (x, y) (x', y')) = concat ["<line x1=", ss x, " y1=", ss y, " x2=", ss x', " y2=", ss y', " stroke-width=\"2\"/>"]
+    where ss = show . show . (*10)
+
+svgWrite :: FilePath -> [Element] -> IO ()
+svgWrite fname elems = writeFile fname contents
+    where contents = concat [ "<svg width=\"12cm\" height=\"10cm\" viewBox=\"0 0 1200 1000\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"
+                            , "<g stroke=\"green\">"
+                            , concatMap svgShow elems
+                            , "</g></svg>" ]
+
+fbShow :: String -> Element -> String
+fbShow factId (Line (x, y) (x', y')) = 
+    concat [ "(", factId, " :LINE-SEGMENT NIL)\n"
+           , "(", factId, " :START (", show x, " ", show y, "))\n"
+           , "(", factId, " :END (", show x', " ", show y', "))\n"]
+
+fbWrite :: FilePath -> [Element] -> IO ()
+fbWrite fname elems = writeFile fname . concat $ map (uncurry fbShow) pairs
+    where pairs = zip (map ((":FACT"++) . show) [1..]) elems
+
+---------- Main and related utility
+processFile :: FilePath -> IO ()
+processFile fname = do f <- readSparse fname
+                       putStrLn $ "\n=>" ++ fname
+                       let elems = align $ getElements 7 f
+                       svgWrite (replaceExtension fname "svg") elems
+                       mapM_ (putStrLn . show) elems
 
 main :: IO ()
-main = do f <- readSparse "test3.txt"
-          let elems = align $ computeFromGrid 7 f
-          putStrLn . show $ map (\(Line a b) -> distance a b) elems
-          svgWrite "test.svg" elems
-          mapM_ (putStrLn . show) elems
+main = mapM_ processFile ["test.txt", "test2.txt", "test3.txt", "multi.txt"]
