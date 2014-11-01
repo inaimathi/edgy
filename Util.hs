80036c51eef4ea067b3055f75defe5283368030a
inaimathi <inaimathi@self>
2014-11-01 10:11:38 -0400

    -Removing some Util functions

--- Util.hs
+++ Util.hs
@@ -1,33 +1,14 @@
-module Util ( iterateM_, putTwoUp
-            , lengthI, sizeI, infI, descending
-            , putBeside, splitEvery
-            , none) where
+module Util ( lengthI, sizeI
+            , putBeside, splitEvery) where
 
-import Data.List (sortBy, intercalate)
+import Data.List (intercalate)
 import Data.Map (Map, size)
-
-descending :: Ord a => [a] -> [a]
-descending = sortBy (flip compare)
 
 lengthI :: [a] -> Integer
 lengthI = toInteger . length
 
 sizeI :: Map k v -> Integer
 sizeI = toInteger . size
-
-putTwoUp :: String -> String -> IO ()
-putTwoUp strA strB = mapM_ (\(a, b) -> putStrLn $ concat [a, " + ", b])
-               $ zip (lines strA) (lines strB)
-
-iterateM_ :: Monad m => (a -> m a) -> a -> m b
-iterateM_ f a = do res <- f a
-                   iterateM_ f res
-
-none :: [Bool] -> Bool
-none = and . map not
-
-infI :: Integer
-infI = toInteger . round $ 1/0
 
 putBeside :: [String] -> IO ()
 putBeside strs = putLns $ map padOut ls
