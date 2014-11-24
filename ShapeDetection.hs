b0de3afc7066896250cc62f89a6c2b33fdd10eba
inaimathi <inaimathi@self>
2014-11-23 20:56:38 -0500

    -Mild tweaking in the ellipse detector

--- ShapeDetection.hs
+++ ShapeDetection.hs
@@ -3,11 +3,13 @@ module ShapeDetection (isEllipse) where
 import Util
 import Model
 
+import SparseRead
+
 pointAt :: (Floating a, RealFrac a) => Coord -> a -> a -> a -> Coord
 pointAt (x, y) t rx ry = (x+(roundI $ rx * (cos t)), y+(roundI $ ry * (sin t)))
 
 ellipsePoints :: Coord -> Integer -> Integer -> [Coord]
-ellipsePoints center rx ry = [pointAt center t rx' ry' | t <- [0,10..360]]
+ellipsePoints center rx ry = [pointAt center t rx' ry' | t <- [0,20..200]]
     where rx' = fromIntegral rx
           ry' = fromIntegral ry
 
@@ -20,10 +22,20 @@ checkResults res = recur res 0
               | otherwise = recur rest $ succ ct
           recur (_:rest) ct = recur rest ct
 
-isEllipse :: Grid Char -> Bool
-isEllipse g = checkResults res
-    where res = map (flip Model.member g) $ ellipsePoints center rx ry
-          (Box a@(x, y) b@(x', y')) = boxOf g
+potentialEllipse :: Grid a -> [Coord]
+potentialEllipse g = ellipsePoints center rx ry
+    where (Box a@(x, y) b@(x', y')) = boxOf g
           center = midpoint a b
           rx = (x' - x) `div` 2
           ry = (y' - y) `div` 2
+
+isEllipse :: Grid a -> Bool
+isEllipse g = checkResults res
+    where res = map (flip Model.member g) $ potentialEllipse g
+
+traceEllipse :: Grid Char -> Grid Char
+traceEllipse g = foldl (\memo pt -> Model.insert pt 'M' memo) g $ potentialEllipse g
+
+-- main :: IO ()
+-- main = do f <- readSparse "test-data/sanitized-ellipse1.ppm"
+--           mapM_ (putStrLn . showCharGrid . traceEllipse) [f]
